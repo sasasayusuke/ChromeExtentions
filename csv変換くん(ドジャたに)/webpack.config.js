876afa810b472546c898e24ce578c426134aa313
sasasa <sasasayusuke@gmail.com>
2024-05-11 20:34:56 +0900

    test commit

---
+++ csv変換くん(ドジャたに)/webpack.config.js
@@ -0,0 +1,22 @@
+const { VueLoaderPlugin } = require('vue-loader');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+};
